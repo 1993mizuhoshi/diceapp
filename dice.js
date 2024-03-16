bde96a3282c5d7baeb4efb9d4d1a5f9d6704bdf6
1993mizuhoshi <161296332+1993mizuhoshi@users.noreply.github.com>
2024-03-16 16:56:53 +0900

    Add files via upload

--- dice.js
+++ dice.js
@@ -4,19 +4,43 @@ const adicebutton = document.getElementById('a');
 const bdicebutton = document.getElementById('b');
 const cdicebutton = document.getElementById('c');
 const ddicebutton = document.getElementById('d');
+const edicebutton = document.getElementById('e');
+const fdicebutton = document.getElementById('f');
+const gdicebutton = document.getElementById('g');
 const custombutton = document.getElementById('cus');
 const resultDivision = document.getElementById('result-area');
+const inputNumber = document.getElementById('ginou');
+
 
 adicebutton.addEventListener(
   'click',
   function generateRandomNumber() {
+      // テキストボックスから取得した数字
+  const userInput = parseInt(inputNumber.value, 10);
+
     // 1から100までのランダムな整数を生成
     const randomNumber = Math.floor(Math.random() * 100) + 1;
   
     // 結果を表示する要素にランダムな値を設定
     document.getElementById('aresult').innerText = `${randomNumber}`;
+
+    // ランダムな値とユーザーの入力を比較
+    if (randomNumber < userInput) {
+      aresult.innerText += ' 成功';
+    } else if (randomNumber > userInput) {
+      aresult.innerText += ' 失敗';
+    } else {
+      aresult.innerText += ' 成功';
+    }
+
+     // クリティカルまたはファンブルの場合の条件判定
+  if (randomNumber >= 95 && randomNumber <= 100) {
+    aresult.innerText += ' ファンブル';
+  } else if (randomNumber >= 1 && randomNumber <= 5) {
+    aresult.innerText += ' クリティカル';
   }
-);
+  
+  });
 
 bdicebutton.addEventListener(
   'click',
@@ -51,6 +75,38 @@ function generateRandomNumber() {
 }
 );
 
+edicebutton.addEventListener(
+  'click',
+  function generateRandomNumber() {
+    // 1から4までのランダムな整数を生成
+    const randomNumber = Math.floor(Math.random() * 4) + 1;
+  
+    // 結果を表示する要素にランダムな値を設定
+    document.getElementById('eresult').innerText = `${randomNumber}`;
+  }
+);
+
+fdicebutton.addEventListener(
+  'click',
+  function generateRandomNumber() {
+    // 1から3までのランダムな整数を生成
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+  
+    // 結果を表示する要素にランダムな値を設定
+    document.getElementById('fresult').innerText = `${randomNumber}`;
+  }
+);
+
+gdicebutton.addEventListener(
+  'click',
+  function generateRandomNumber() {
+    // 1から8までのランダムな整数を生成
+    const randomNumber = Math.floor(Math.random() * 8) + 1;
+  
+    // 結果を表示する要素にランダムな値を設定
+    document.getElementById('gresult').innerText = `${randomNumber}`;
+  }
+);
 
 custombutton.addEventListener('click', function generateRandomNumbers() {
   // テキストボックスから回数を取得
